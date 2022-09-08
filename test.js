d016dec22d19d9ed60e7d6cff8a3cb01c38c4044
Wi JungWoo <wirush4020@gmail.com>
2022-09-08 18:40:33 +0900

    1.0

---
+++ test.js
@@ -0,0 +1,10 @@
+function findEvenNumber() {
+    let result = []
+    for(let i = 1; i <= 50; i++) {
+        if (i%2 === 0) {
+            result.push(i);
+        }
+    }
+    return result;
+}
+console.log(findEvenNumber());
